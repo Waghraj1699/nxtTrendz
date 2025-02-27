c29eac587b7ed4f70f4283d2477c0178be20d767
waghraj1699 <waghrajpatil010699@gmail.com>
2025-02-27 13:15:47 +0000

    updatedCode

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,3 +1,7 @@
+import Popup from 'reactjs-popup'
+
+import Payment from '../Payment'
+
 import CartContext from '../../context/CartContext'
 
 import './index.css'
@@ -19,13 +23,19 @@ const CartSummary = () => (
               /-
             </h1>
             <p className="total-items">{cartList.length} Items in cart</p>
-            <button type="button" className="checkout-button d-sm-none">
-              Checkout
-            </button>
+
+            <Popup
+              modal
+              trigger={
+                <button className="checkout-btn" type="button">
+                  Checkout
+                </button>
+              }
+              position="top left"
+            >
+              {close => <Payment close={close} />}
+            </Popup>
           </div>
-          <button type="button" className="checkout-button d-lg-none">
-            Checkout
-          </button>
         </>
       )
     }}
